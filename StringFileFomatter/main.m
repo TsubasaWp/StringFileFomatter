9a26ae96dd6408aec027ecb5d449ab739057f41b
wang pei <tsubasawang@tencent.com>
2013-11-18 15:41:32 +0800

    Initial Commit

---
+++ StringFileFomatter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  StringFileFomatter
+//
+//  Created by tsubasa on 13-11-18.
+//  Copyright (c) 2013年 tsubasa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
